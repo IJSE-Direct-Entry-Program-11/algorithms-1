28d360af99094cda4f00f5754d15eb7f7f09e643
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-08-12 12:01:16 +0530

    Initial commit

---
+++ LinearSearchAlgo2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LinearSearchAlgo2 {
+    private final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        String[] names = {"Kasun", "Nuwan", "Ruwan", "Supun", "Upul", "Nimal"};
+
+        loop:
+        do {
+            System.out.print("Enter a name to search: ");
+            String search = scanner.nextLine();
+
+            for (int i = 0; i < names.length; i++) {
+                if (names[i].equalsIgnoreCase(search)){
+                    System.out.println("Found at index: " + i);
+                    continue loop;
+                }
+            }
+            System.out.println("Not found!");
+        }while (true);
+    }
+}
---
+++ utils/StringUtils.java
@@ -0,0 +1,8 @@
+public class StringUtils {
+    public static void main(String[] args) {
+        String something = "Hello";
+        System.out.println(something.contains("H"));
+        System.out.println(something.contains("a"));
+        System.out.println(something.contains("e"));
+    }
+}
